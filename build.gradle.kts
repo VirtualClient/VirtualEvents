7d234ebadebacbd97ee94554b57d9e35a7109a1a
David Mayr <davidliebtkekse@gmail.com>
2022-07-12 15:54:32 +0200

    Added Maven publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.7.0"
+    id("maven-publish")
 }
 
 group = "gg.virtualclient"
@@ -14,6 +15,26 @@ repositories {
 dependencies {
     testImplementation(kotlin("test"))
 }
+
+configure<PublishingExtension> {
+    repositories {
+        maven {
+            name = "virtualclientRepository"
+            credentials(PasswordCredentials::class)
+            url = uri("https://repo.virtualclient.gg/artifactory/virtualclient-public/")
+        }
+    }
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "gg.virtualclient"
+            artifactId = "VirtualEvents"
+            version = "1.0-SNAPSHOT"
+            from(components["java"])
+        }
+    }
+}
+
+
 
 tasks.test {
     useJUnitPlatform()
